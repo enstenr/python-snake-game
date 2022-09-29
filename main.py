74b6e1796617a27e00ad96bfcfd2feefd3eb6525
enstenr <enstenr@gmail.com>
2022-09-29 18:49:38 +0530

    Added method for creating snake and food

--- main.py
+++ main.py
@@ -1,89 +1,143 @@
 from tkinter import *
 import random
 from config import config as cfg
-GAME_WIDTH=cfg.GAME_WIDTH
-GAME_HEIGHT=cfg.GAME_HEIGHT
-SPEED=cfg.SPEED
-SPACE_SIZE=cfg.SPACE_SIZE
-BODY_PARTS=cfg.BODY_PARTS
-SNAKE_COLOR=cfg.SNAKE_COLOR
-FOOD_COLOR=cfg.FOOD_COLOR
-BACKGROUND_COLOR=cfg.BACKGROUND_COLOR
+
+GAME_WIDTH = cfg.GAME_WIDTH
+GAME_HEIGHT = cfg.GAME_HEIGHT
+SPEED = cfg.SPEED
+SPACE_SIZE = cfg.SPACE_SIZE
+BODY_PARTS = cfg.BODY_PARTS
+SNAKE_COLOR = cfg.SNAKE_COLOR
+FOOD_COLOR = cfg.FOOD_COLOR
+BACKGROUND_COLOR = cfg.BACKGROUND_COLOR
+
 
 class Snake:
-    def __int__(self):
-        self.body_size=BODY_PARTS
-        self.coordinates=[]
-        self.squares=[]
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
 
         for i in range(0, BODY_PARTS):
             self.coordinates.append([0, 0])
 
-        for x,y in self.coordinates:
-            squares=canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOR,tag="snake")
+        for x, y in self.coordinates:
+            squares = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(squares)
+
+
 class Food:
+    def __init__(self):
 
-    def __int__(self):
-        print('inside')
-        x = random.randint(0,(GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
         y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
-        self.coordinates=[x,y]
-        print(x,y)
-        print(x,y)
-        canvas.create_oval(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=FOOD_COLOR,tag="food")
-def next_turn(snake,food):
-    x,y=snake.coordinates[0]
+        self.coordinates = [x, y]
 
-    if direction=="up":
-        y-=SPACE_SIZE
-    elif direction=="down":
-        y+=SPACE_SIZE
-    elif direction=="left":
-        x-=SPACE_SIZE
-    elif direction=="right":
-        x+=SPACE_SIZE
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+def next_turn(snake, food):
+
+    x, y = snake.coordinates[0]
+
+    if direction == "up":
+        y -= SPACE_SIZE
+    elif direction == "down":
+        y += SPACE_SIZE
+    elif direction == "left":
+        x -= SPACE_SIZE
+    elif direction == "right":
+        x += SPACE_SIZE
 
     snake.coordinates.insert(0, (x, y))
-    square=canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOR)
-    snake.squares.insert(0,square)
-    window.after(SPEED,next_turn,snake,food)
-def change_direction(new_direction):
-    pass
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+    snake.squares.insert(0, square)
 
-def check_collision():
-    pass
+    if x==food.coordinates[0] and y== food.coordinates[1]:
+        global score
+        score +=1
+        label.config(text=" Score:{}".format(score))
+        canvas.delete("food")
+        food = Food()
+    else:
+
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
+
+    if check_collision(snake):
+        game_over()
+    else:
+        window.after(SPEED, next_turn, snake, food)
+
+
+def change_direction(new_direction):
+    global direction
+    if new_direction=='left':
+        if direction!='right':
+            direction=new_direction
+    if new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    if new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+
+    if new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+
+
+def check_collision(snake):
+    x,y=snake.coordinates[0]
+    if x<0 or x>= cfg.GAME_WIDTH:
+        return True
+    elif y<0 or y>=cfg.GAME_HEIGHT:
+        print("GAME OVER")
+        return True
+    for body_part in snake.coordinates[1:]:
+        if x==body_part[0] and y==body_part[1]:
+            print("GAME OVER")
+            return True
+    return False
 
 def game_over():
-    pass
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2,canvas.winfo_height()/2, font=('consolas',70),text="GAME OVER",fill="purple")
+
 
 if __name__ == '__main__':
-    window=Tk()
+    window = Tk()
     window.title("Snake Game")
-    window.resizable(False,False)
+    window.resizable(False, False)
 
-    score=0
-    direction='down'
+    score = 0
+    direction = 'down'
 
-    label=Label(window,text="Score:{}".format(score),font=('consolas',40))
+    label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
     label.pack()
 
-    canvas=Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
     canvas.pack()
     window.update()
 
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
-    window_width=window.winfo_width()
-    window_height=window.winfo_height()
-    screen_width=window.winfo_screenwidth()
-    screen_height=window.winfo_screenheight()
-
-    x=int((screen_width/2)-(window_width/2))
-    y=int((screen_height/2)-(window_height/2))
+    x = int((screen_width / 2) - (window_width / 2))
+    y = int((screen_height / 2) - (window_height / 2))
 
     window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    window.bind('<Left>',lambda event:change_direction('left'))
+    window.bind('<Right>', lambda event: change_direction('right'))
+    window.bind('<Up>', lambda event: change_direction('up'))
+    window.bind('<Down>', lambda event: change_direction('down'))
 
-   # snake=Snake()
-    food=Food()
-   # next_turn(snake,food)
+
+    snakeobj=Snake()
+    foodobj = Food()
+    print(foodobj)
+    next_turn(snakeobj,foodobj)
     window.mainloop()
